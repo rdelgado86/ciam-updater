7bfe31867229e185ce6b288f3ad6ab01e5d784b8
Ricardo <ricardomdelgado@gmail.com>
2023-08-10 23:50:27 -0300

    initial Commit

---
+++ src/main/java/com/pluxee/ciamupdater/CiamUpdaterApplication.java
@@ -0,0 +1,13 @@
+package com.pluxee.ciamupdater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiamUpdaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiamUpdaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pluxee/ciamupdater/CiamUpdaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluxee.ciamupdater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiamUpdaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
